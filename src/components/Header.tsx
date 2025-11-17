e40293c0d9d43783a78f1058e2edc13f231cb5e8
Gin-Z <gonzaloperez199@outlook.com>
2025-11-17 16:38:10 -0300

    Primer commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import type { TodoTitle } from "../types"
+import { CreateTodo } from "./CreateTodo"
+
+interface Props{
+    saveTodo:({title}:TodoTitle)=>void
+}
+
+export const Header: React.FC<Props>=({saveTodo})=>{
+    return (
+        <header className="header">
+            <h1>Todo Typescript</h1>
+            <CreateTodo saveTodo={saveTodo}/>
+        </header>
+    )
+}
