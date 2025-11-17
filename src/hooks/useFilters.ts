d51e9daff2835e06e997fa1cddb7487e1021630b
Gin-Z <gonzaloperez199@outlook.com>
2025-11-17 17:57:04 -0300

    Commit 3: Se creó la carpeta hooks con los archivos useFilters.ts y useTodos.ts. Se modificó el archivo App.tsx

---
+++ src/hooks/useFilters.ts
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import { type FilterValue, type Todo } from "../types"
+import { TODO_FILTERS } from "../consts"
+
+export const useFilters = (todos: Todo[]) => {
+  const [filterSelected, setFilterSelected] = useState<FilterValue>(TODO_FILTERS.ALL)
+
+  const handleFilterChange = (filter: FilterValue): void => {
+    setFilterSelected(filter)
+  }
+
+  const filteredTodos = todos.filter(todo => {
+    if (filterSelected === TODO_FILTERS.ACTIVE) return !todo.completed
+    if (filterSelected === TODO_FILTERS.COMPLETED) return todo.completed
+    return true
+  })
+
+  const activeCount = todos.filter(todo => !todo.completed).length
+  const completedCount = todos.length - activeCount
+
+  return {
+    filterSelected,
+    handleFilterChange,
+    filteredTodos,
+    activeCount,
+    completedCount
+  }
+}
---
+++ src/hooks/useTodos.ts
@@ -0,0 +1,79 @@
+import { useState } from "react"
+import { type Todo, type TodoId, type TodoTitle } from "../types"
+
+const mockTodos: Todo[] = [
+  {
+    id: "1",
+    title: "Ir al cine.",
+    completed: true
+  },
+  {
+    id: "2",
+    title: "Aprender React con Typescript.",
+    completed: false
+  },
+  {
+    id: "3",
+    title: "Bañarse.",
+    completed: false
+  }
+]
+
+export const useTodos = () => {
+  const [todos, setTodos] = useState(mockTodos)
+
+  const handleRemove = ({ id }: TodoId): void => {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
+
+  const handleCompleted = ({ id, completed }: Pick<Todo, 'id' | 'completed'>): void => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed
+        }
+      }
+      return todo
+    })
+    setTodos(newTodos)
+  }
+
+  const handleUpdateTitle = ({ id, title }: Pick<Todo, 'id' | 'title'>): void => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          title
+        }
+      }
+      return todo
+    })
+    setTodos(newTodos)
+  }
+
+  const handleRemoveCompleted = (): void => {
+    const newTodos = todos.filter(todo => !todo.completed)
+    setTodos(newTodos)
+  }
+
+  const handleAddTodo = ({ title }: TodoTitle): void => {
+    const newTodo: Todo = {
+      title,
+      id: crypto.randomUUID(),
+      completed: false
+    }
+    const newTodos = [...todos, newTodo]
+    setTodos(newTodos)
+  }
+
+  return {
+    todos,
+    handleRemove,
+    handleCompleted,
+    handleUpdateTitle,
+    handleRemoveCompleted,
+    handleAddTodo
+  }
+}
